aecd6e51f4783b5a517f8d03db02b679faef580b
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-06-01 19:31:57 +0700

    add server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/api/v1/users', authRoutes);
+app.get('/', (req, res) => {
+  res.send('App is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
