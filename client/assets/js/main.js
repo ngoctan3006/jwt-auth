4fcb217fea3bd491e3e0b7daf3fe95fdc0a35895
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-06-08 03:10:41 +0700

    sửa lại giao diện

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,27 +1,29 @@
 function signup(e) {
   e.preventDefault();
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const username = document.querySelector('input[name="username"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+  const cfpassword = document.querySelector('input[name="cfpassword"]').value;
   const user = {
     username,
     email,
     password,
+    cfpassword,
   };
-  localStorage.setItem(username, JSON.stringify(user));
+  const json = JSON.stringify(user);
+  localStorage.setItem(username, json);
   alert('Đăng ký thành công');
 }
 
 function login(e) {
   e.preventDefault();
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const username = document.querySelector('input[name="username"]').value;
+  const password = document.querySelector('input[name="password"]').value;
   const user = localStorage.getItem(username);
   const data = JSON.parse(user);
   if (!user) {
     alert('Vui lòng nhập thông tin');
-  } else if (username == data.username && email == data.email && password == data.password) {
+  } else if (username == data.username && password == data.password) {
     alert('Đăng nhập thành công');
     window.location.href = '';
   } else {
