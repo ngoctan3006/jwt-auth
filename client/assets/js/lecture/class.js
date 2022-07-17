2fbc8ed2cc07ac149d53f21c42f0e540256add81
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-07-18 05:21:04 +0700

    update class

---
+++ client/assets/js/lecture/class.js
@@ -0,0 +1,107 @@
+let classList = [];
+let currClass = null;
+
+const editClass = (id) => {
+  currClass = id;
+  const data = classList.find((item) => item.id === id);
+  try {
+    console.log(data);
+    $('#edit-class-code').val(data.code);
+    $('#edit-subject-name').val(data.subjectName);
+    $('#edit-semester').val(data.semester);
+    $('#edit-room').val(data.room);
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+};
+
+const updateClass = async (id) => {
+  const oldClass = classList.find((item) => item.id === id);
+  const classCode = $('#edit-class-code').val();
+  const subjectName = $('#edit-subject-name').val();
+  const semester = $('#edit-semester').val();
+  const room = $('#edit-room').val();
+  const newClass = { ...oldClass, classCode, subjectName, semester, room };
+  classList = classList.map((item) => (item.id === id ? newClass : item));
+  try {
+    await API.put(`/class/${id}`, newClass);
+    alert('Cập nhật thành công!');
+    $('#editClassModal').modal('hide');
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+  renderClassList();
+};
+
+const renderClassList = () => {
+  const classListHtml = classList
+    .map(
+      ({ id, code, room, semester, subjectName }) => `
+      <tr id=${code}>
+        <td><a href="class-detail.html?code=${code}">${code}</a></td>
+        <td>${subjectName}</td>
+        <td>${semester}</td>
+        <td>${room}</td>
+        <td>
+          <button
+            class="btn btn-warning"
+            data-toggle="modal"
+            data-target="#editClassModal"
+            onclick="editClass('${id}')"
+          >
+            <i class="fa fa-fw fa-edit"></i>
+          </button>
+          <button class="btn btn-danger" onclick="deleteClass(this)">
+            <i class="fa fa-fw fa-trash"></i>
+          </button>
+        </td>
+      </tr>
+    `
+    )
+    .join('');
+
+  $('#class-list-body').html(classListHtml);
+};
+
+(async () => {
+  try {
+    const { data } = await API.get('/class');
+    classList.push(...data);
+    if (classList.length) {
+      $('#no-data').hide();
+      $('#class-list').show();
+      renderClassList();
+    } else {
+      $('#no-data').show();
+      $('#class-list').hide();
+    }
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+})();
+
+const addClass = async () => {
+  const classCode = $('#class-code').val();
+  const subjectCode = $('#subject-code').val();
+  const subjectName = $('#subject-name').val();
+  const semester = $('#semester').val();
+  const room = $('#room').val();
+
+  if (!classCode || !subjectCode || !subjectName || !semester || !room) {
+    alert('Vui lòng nhập đầy đủ thông tin!');
+    return;
+  }
+
+  const data = { classCode, subjectCode, subjectName, semester, room };
+
+  try {
+    const {
+      data: { message },
+    } = await API.post('/class', data);
+    alert(message);
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+
+  $('#addClassModal').modal('hide');
+};
